a15cec74b6c282f49f1f1305936ced6116838fb2
Matthew Caryl <matthew.caryl@permutationcity.co.uk>
2024-07-16 21:47:06 +0100

    Multi-bit manipulation library
    This provides single functions to check, set or flip multiple consecutive bits.
    
    fix incorrect return type

--- src/Bits.hpp
+++ src/Bits.hpp
@@ -20,6 +20,25 @@ namespace Tier0::Bits
 	}
 
 	template <typename Value>
+	Value MaskUpTo(uint8_t index)
+	{
+		return (1u << index) - 1;
+	}
+
+	template <typename Value>
+	Value Mask(uint8_t index, uint8_t size)
+	{
+		if (index == 0)
+		{
+			return MaskUpTo<Value>(size);
+		}
+		else
+		{
+			return MaskUpTo<Value>(index + size) & ~MaskUpTo<Value>(index);
+		}
+	}
+
+	template <typename Value>
 	bool IsClear(Value value, uint8_t index)
 	{
 		return !IsSet<Value>(value, index);
@@ -32,9 +51,36 @@ namespace Tier0::Bits
 	}
 
 	template <typename Value>
-	Value Clear(Value value, uint8_t index)
+	bool IsAllSet(Value value, uint8_t index, uint8_t size)
+	{
+		const auto mask = Mask<Value>(index, size);
+		return (value & mask) == mask;
+	}
+
+	template <typename Value>
+	bool IsAnySet(Value value, uint8_t index, uint8_t size)
+	{
+		const auto mask = Mask<Value>(index, size);
+		return (value & mask) != 0;
+	}
+
+	template <typename Value>
+	bool IsNoneSet(Value value, uint8_t index, uint8_t size)
+	{
+		const auto mask = Mask<Value>(index, size);
+		return (value & mask) == 0;
+	}
+
+	template <typename Value>
+	uint32_t Clear(Value value, uint8_t index)
 	{
 		return value & ~Mask<Value>(index);
+	}
+
+	template <typename Value>
+	Value Clear(Value value, uint8_t index, uint8_t size)
+	{
+		return value & ~Mask<Value>(index, size);
 	}
 
 	template <typename Value>
@@ -44,14 +90,20 @@ namespace Tier0::Bits
 	}
 
 	template <typename Value>
-	Value Set(Value value, uint8_t index, bool set)
+	Value Set(Value value, uint8_t index, uint8_t size)
 	{
-		return set ? Set(value, index) : Clear(value, index);
+		return value | Mask<Value>(index, size);
 	}
 
 	template <typename Value>
 	Value Flip(Value value, uint8_t index)
 	{
 		return value ^ Mask<Value>(index);
+	}
+
+	template <typename Value>
+	Value Flip(Value value, uint8_t index, uint8_t size)
+	{
+		return value ^ Mask<Value>(index, size);
 	}
 }
