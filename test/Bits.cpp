8979d1cce1b26920567ccc073f5f8d663ba39a5f
Matthew Caryl <matthew.caryl@permutationcity.co.uk>
2024-07-16 21:34:52 +0100

    fix identifer change

--- test/Bits.cpp
+++ test/Bits.cpp
@@ -33,8 +33,8 @@ TEST(Bits, Mask)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
-		auto bitset = GetBitset(i);
-		EXPECT_EQ(Bits::Mask<Value>(i), bitset.to_ullong());
+		auto bit = GetBitset(i);
+		EXPECT_EQ(Bits::Mask<Value>(i), bit.to_ullong());
 	}
 }
 
@@ -42,9 +42,9 @@ TEST(Bits, IsClear)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
-		auto bitset = GetBitset(i);
-		EXPECT_TRUE(Bits::IsClear<Value>(~bitset.to_ullong(), i));
-		EXPECT_FALSE(Bits::IsClear<Value>(~bitset.to_ullong(), i));
+		auto bit = GetBitset(i);
+		EXPECT_TRUE(Bits::IsClear<Value>(~bit.to_ullong(), i));
+		EXPECT_FALSE(Bits::IsClear<Value>(~bit.to_ullong(), i));
 	}
 }
 
@@ -52,9 +52,9 @@ TEST(Bits, IsSet)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
-		auto bitset = GetBitset(i);
-		EXPECT_TRUE(Bits::IsSet<Value>(bitset.to_ullong(), i));
-		EXPECT_FALSE(Bits::IsSet<Value>(~bitset.to_ullong(), i));
+		auto bit = GetBitset(i);
+		EXPECT_TRUE(Bits::IsSet<Value>(bit.to_ullong(), i));
+		EXPECT_FALSE(Bits::IsSet<Value>(~bit.to_ullong(), i));
 	}
 }
 
@@ -62,9 +62,9 @@ TEST(Bits, Set)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
-		auto bitset = GetBitset(i);
-		EXPECT_EQ(Bits::Set<Value>(0u, i), bitset.to_ullong());
-		EXPECT_EQ(Bits::Set<Value>(bitset.to_ullong(), i), bitset.to_ullong());
+		auto bit = GetBitset(i);
+		EXPECT_EQ(Bits::Set<Value>(0u, i), bit.to_ullong());
+		EXPECT_EQ(Bits::Set<Value>(bit.to_ullong(), i), bit.to_ullong());
 	}
 }
 
@@ -72,10 +72,10 @@ TEST(Bits, Clear)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
-		auto bitset = GetBitset(i);
-		EXPECT_EQ(Bits::Clear<Value>(bitset.to_ullong(), i), 0u);
-		bitset.flip();
-		EXPECT_EQ(Bits::Clear<Value>(bitset.to_ullong(), i), bitset.to_ullong());
+		auto bit = GetBitset(i);
+		EXPECT_EQ(Bits::Clear<Value>(bit.to_ullong(), i), 0u);
+		bit.flip();
+		EXPECT_EQ(Bits::Clear<Value>(bit.to_ullong(), i), bit.to_ullong());
 	}
 }
 
@@ -83,8 +83,8 @@ TEST(Bits, Flip)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
-		auto bitset = GetBitset(i);
-		EXPECT_EQ(Bits::Flip<Value>(0u, i), bitset.to_ullong());
-		EXPECT_EQ(Bits::Flip<Value>(bitset.to_ullong(), i), 0u);
+		auto bit = GetBitset(i);
+		EXPECT_EQ(Bits::Flip<Value>(0u, i), bit.to_ullong());
+		EXPECT_EQ(Bits::Flip<Value>(bit.to_ullong(), i), 0u);
 	}
 }
