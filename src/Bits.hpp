a205f5c56ef852482fd3589111342b1871d6264a
Matthew Caryl <matthew.caryl@permutationcity.co.uk>
2024-07-16 19:50:00 +0100

    Single bit manipulation library
    This provides single functions to check, set or flip individual bits.

---
+++ src/Bits.hpp
@@ -0,0 +1,57 @@
+//
+// Bits.hpp
+//
+// Copyright Matthew Caryl 2024
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <cstdint>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Tier0::Bits
+{
+	template <typename Value>
+	Value Mask(uint8_t index)
+	{
+		return 1u << index;
+	}
+
+	template <typename Value>
+	bool IsClear(Value value, uint8_t index)
+	{
+		return !IsClear(value, index);
+	}
+
+	template <typename Value>
+	bool IsSet(Value value, uint8_t index)
+	{
+		return value & Mask<Value>(index);
+	}
+
+	template <typename Value>
+	uint32_t Clear(Value value, uint8_t index)
+	{
+		return value & ~Mask<Value>(index);
+	}
+
+	template <typename Value>
+	uint32_t Set(Value value, uint8_t index)
+	{
+		return value | Mask<Value>(index);
+	}
+
+	template <typename Value>
+	uint32_t Set(Value value, uint8_t index, bool set)
+	{
+		return set ? Set(value, index) : Clear(value, index);
+	}
+
+	template <typename Value>
+	uint32_t Flip(Value value, uint8_t index)
+	{
+		return Set(value, index, !IsSet(value, index));
+	}
+}
---
+++ test/Bits.cpp
@@ -0,0 +1,89 @@
+//
+// Bits.cpp
+//
+// Copyright Matthew Caryl 2024
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Bits.hpp"
+
+#include <bitset>
+
+#include <gtest/gtest.h>
+
+using namespace Tier0;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using Value = uint32_t;
+static const uint8_t sc_Size = 32;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static std::bitset<sc_Size> GetBitset(uint8_t index)
+{
+	std::bitset<sc_Size> bitset;
+	bitset[index] = true;
+	return bitset;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST(Bits, Mask)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		EXPECT_EQ(Bits::Mask<Value>(i), 1u << i);
+	}
+}
+
+TEST(Bits, IsClear)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bitset = GetBitset(i);
+		EXPECT_TRUE(Bits::IsClear<Value>(~bitset.to_ullong(), i));
+		EXPECT_FALSE(Bits::IsClear<Value>(~bitset.to_ullong(), i));
+	}
+}
+
+TEST(Bits, IsSet)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bitset = GetBitset(i);
+		EXPECT_TRUE(Bits::IsSet<Value>(bitset.to_ullong(), i));
+		EXPECT_FALSE(Bits::IsSet<Value>(~bitset.to_ullong(), i));
+	}
+}
+
+TEST(Bits, Set)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bitset = GetBitset(i);
+		EXPECT_EQ(Bits::Set<Value>(0u, i), bitset.to_ullong());
+		EXPECT_EQ(Bits::Set<Value>(bitset.to_ullong(), i), bitset.to_ullong());
+	}
+}
+
+TEST(Bits, Unset)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bitset = GetBitset(i);
+		EXPECT_EQ(Bits::Clear<Value>(bitset.to_ullong(), i), 0u) << i << ":" << bitset;
+		bitset.flip();
+		EXPECT_EQ(Bits::Clear<Value>(bitset.to_ullong(), i), bitset.to_ullong()) << i << ":" << bitset;
+	}
+}
+
+TEST(Bits, Flip)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bitset = GetBitset(i);
+		EXPECT_EQ(Bits::Flip<Value>(0u, i), bitset.to_ullong()) << i << ":" << bitset;
+		EXPECT_EQ(Bits::Flip<Value>(bitset.to_ullong(), i), 0u) << i << ":" << bitset;
+	}
+}
