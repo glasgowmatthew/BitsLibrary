fde027c95afe406b73d43e38112937808ca95831
Matthew Caryl <matthew.caryl@permutationcity.co.uk>
2024-08-04 19:37:53 +0100

    Multi-bit manipulation library
    This provides single functions to check, set or flip multiple consecutive bits.
    Also access smaller bit fields within a value.

--- src/Bits.hpp
+++ src/Bits.hpp
@@ -8,48 +8,84 @@
 #pragma once
 
 #include <cstdint>
+#include <limits>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 namespace Tier0::Bits
 {
+	template <typename Value>
+	constexpr uint8_t BitCount()
+	{
+		return sizeof(Value) * 8;
+	}
+	
     template <typename Value>
     constexpr Value Mask(uint8_t index)
     {
         return 1u << index;
     }
 
-    template <typename Value>
-    constexpr bool IsClear(Value value, uint8_t index)
-    {
-        return !IsSet<Value>(value, index);
-    }
+	template <typename Value>
+	constexpr Value MaskUpTo(uint8_t index)
+	{
+		return std::numeric_limits<Value>::max() >> (BitCount<Value>() - 1 - index);
+	}
 
-    template <typename Value>
-    constexpr bool IsSet(Value value, uint8_t index)
-    {
+	template <typename Value>
+	constexpr bool IsAllSet(Value value, uint8_t index, uint8_t size = 1)
+	{
 		const auto mask = Mask<Value>(index);
-        return value & mask;
-    }
+        return (value & mask) == mask;
+	}
+
+	template <typename Value>
+	constexpr bool IsAnySet(Value value, uint8_t index, uint8_t size = 1)
+	{
+		const auto mask = Mask<Value>(index);
+        return (value & mask) != 0;
+	}
+
+	template <typename Value>
+	constexpr bool IsNoneSet(Value value, uint8_t index, uint8_t size = 1)
+	{
+		const auto mask = Mask<Value>(index);
+        return (value & mask) == 0;
+	}
 
     template <typename Value>
-    constexpr Value Clear(Value value, uint8_t index)
+	constexpr Value Clear(Value value, uint8_t index, uint8_t size = 1)
     {
 		const auto mask = Mask<Value>(index);
         return value & ~mask;
     }
 
     template <typename Value>
-    constexpr Value Set(Value value, uint8_t index)
+    constexpr Value Set(Value value, uint8_t index, uint8_t size = 1)
     {
 		const auto mask = Mask<Value>(index);
         return value | mask;
     }
 
     template <typename Value>
-    constexpr Value Flip(Value value, uint8_t index)
+    constexpr Value Flip(Value value, uint8_t index, uint8_t size = 1)
     {
 		const auto mask = Mask<Value>(index);
         return value ^ mask;
+    }
+	
+    template <typename Value>
+    constexpr Value GetAt(Value value, uint8_t index, uint8_t size)
+    {
+        const auto mask = Mask<Value>(index, size);
+        return (value & mask) >> index;
+    }
+
+    template <typename Value>
+    constexpr Value SetAt(Value value, uint8_t index, uint8_t size, Value subValue)
+    {
+        const auto mask = Mask<Value>(index, size);
+        subValue = subValue << index;
+        return (value & ~mask) | (subValue & mask);
     }
 }
--- test/Bits.cpp
+++ test/Bits.cpp
@@ -24,21 +24,56 @@ static const uint8_t sc_TestCount = 100;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static Value GetBit(uint8_t index)
+static Value GetBits(uint8_t index, uint8_t size)
 {
 	Bitset bitset;
-	bitset[index] = true;
+	for (uint8_t i = 0; i < size; i++)
+	{
+		bitset[index + i] = true;
+	}
 	return bitset.to_ullong();
+}
+
+static Value GetBit(uint8_t index)
+{
+	return GetBits(index, 1);
+}
+
+static Value BitsetGetAt(Value value, uint8_t index, uint8_t size)
+{
+	Bitset bits(value);
+	bits >>= index;
+	for (uint8_t i = size; i < bits.size(); i++)
+	{
+		bits[i] = false;
+	}
+	return static_cast<Value>(bits.to_ullong());
+}
+
+static Value SetBitsetAt(Value value, uint8_t index, uint8_t size, Value subValue)
+{
+	Bitset bits(value);
+	Bitset subBits(subValue);
+	for (uint8_t i = 0; i < size; i++)
+	{
+		bits[index + i] = subBits[i];
+	}
+	return static_cast<Value>(bits.to_ullong());
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-TEST(Bits, GetBit)
+TEST(Bits, GetBitset)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
 		auto bit = GetBit(i);
 		EXPECT_EQ(bit, 1u << i);
+	}
+	for (uint8_t i = 0; i < sc_Size - 1; i++)
+	{
+		auto bits = GetBits(i, 2);
+		EXPECT_EQ(bits, 3u << i);
 	}
 }
 
@@ -47,27 +82,71 @@ TEST(Bits, Mask)
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
 		auto bit = GetBit(i);
-		EXPECT_EQ(Bits::Mask<Value>(i), bit);
+		EXPECT_EQ(Bits::Mask<Value>(i), bit) << std::to_string(i);
+
+		for (uint8_t j = 0; i + j < sc_Size; j++)
+		{
+			auto bits = GetBits(i, j);
+			EXPECT_EQ(Bits::Mask<Value>(i, j), bits) << std::to_string(i);
+		}
 	}
 }
 
-TEST(Bits, IsSet)
+TEST(Bits, MaskUpTo)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bits = GetBits(0, i + 1);
+		EXPECT_EQ(Bits::MaskUpTo<Value>(i), bits);
+	}
+}
+
+TEST(Bits, IsAllSet)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
 		auto bit = GetBit(i);
 		EXPECT_TRUE(Bits::IsAllSet<Value>(bit, i));
 		EXPECT_FALSE(Bits::IsAllSet<Value>(~bit, i));
+
+		for (uint8_t j = 0; i + j < sc_Size; j++)
+		{
+			auto bits = GetBits(i, j);
+			EXPECT_TRUE(Bits::IsAllSet<Value>(bits, i, j));
+			EXPECT_TRUE(Bits::IsAllSet<Value>(~bits, 0, i));
+			EXPECT_TRUE(Bits::IsAllSet<Value>(~bits, i + j, sc_Size - i - j));
+		}
 	}
 }
 
-TEST(Bits, IsClear)
+TEST(Bits, IsAnySet)
 {
 	for (uint8_t i = 0; i < sc_Size; i++)
 	{
 		auto bit = GetBit(i);
-		EXPECT_TRUE(Bits::IsClear<Value>(~bit, i));
-		EXPECT_FALSE(Bits::IsClear<Value>(bit, i));
+		// We must check at least one bit for IsAnySet to be true.
+		for (uint8_t j = 1; i + j < sc_Size; j++)
+		{
+			EXPECT_TRUE(Bits::IsAnySet<Value>(bit, i, j));
+		}
+	}
+}
+
+TEST(Bits, IsNoneSet)
+{
+	for (uint8_t i = 0; i < sc_Size; i++)
+	{
+		auto bit = GetBit(i);
+		EXPECT_TRUE(Bits::IsNoneSet<Value>(~bit, i));
+		EXPECT_FALSE(Bits::IsNoneSet<Value>(bit, i));
+
+		for (uint8_t j = 0; i + j < sc_Size; j++)
+		{
+			auto bits = GetBits(i, j);
+			EXPECT_TRUE(Bits::IsNoneSet<Value>(~bits, i, j));
+			EXPECT_TRUE(Bits::IsNoneSet<Value>(bits, 0, i));
+			EXPECT_TRUE(Bits::IsNoneSet<Value>(bits, i + j, sc_Size - i - j));
+		}
 	}
 }
 
@@ -78,6 +157,13 @@ TEST(Bits, Set)
 		auto bit = GetBit(i);
 		EXPECT_EQ(Bits::Set<Value>(0u, i), bit);
 		EXPECT_EQ(Bits::Set<Value>(bit, i), bit);
+
+		for (uint8_t j = 0; i + j < sc_Size; j++)
+		{
+			auto bits = GetBits(i, j);
+			EXPECT_EQ(Bits::Set<Value>(0u, i, j), bits);
+			EXPECT_EQ(Bits::Set<Value>(bits, i, j), bits);
+		}
 	}
 }
 
@@ -88,6 +174,13 @@ TEST(Bits, Clear)
 		auto bit = GetBit(i);
 		EXPECT_EQ(Bits::Clear<Value>(bit, i), 0u);
 		EXPECT_EQ(Bits::Clear<Value>(~bit, i), static_cast<Value>(~bit));
+
+		for (uint8_t j = 0; i + j < sc_Size; j++)
+		{
+			auto bits = GetBits(i, j);
+			EXPECT_EQ(Bits::Clear<Value>(bits, i, j), 0u);
+			EXPECT_EQ(Bits::Clear<Value>(~bits, i, j), static_cast<Value>(~bits));
+		}
 	}
 }
 
@@ -98,5 +191,39 @@ TEST(Bits, Flip)
 		auto bit = GetBit(i);
 		EXPECT_EQ(Bits::Flip<Value>(0u, i), bit);
 		EXPECT_EQ(Bits::Flip<Value>(bit, i), 0u);
+
+		for (uint8_t j = 0; i + j < sc_Size; j++)
+		{
+			auto bits = GetBits(i, j);
+			EXPECT_EQ(Bits::Flip<Value>(0u, i, j), bits);
+			EXPECT_EQ(Bits::Flip<Value>(bits, i, j), 0u);
+		}
+	}
+}
+
+TEST(Bits, GetAt)
+{
+	std::mt19937 gen32;
+
+	for (uint8_t i = 0; i < sc_TestCount; i++)
+	{
+		Value value = gen32();
+		uint8_t index = std::uniform_int_distribution<>(0, 31)(gen32);
+		uint8_t size = std::uniform_int_distribution<>(1, sc_Size - index)(gen32);
+		EXPECT_EQ(Bits::GetAt(value, index, size), BitsetGetAt(value, index, size));
+	}
+}
+
+TEST(Bits, SetAt)
+{
+	std::mt19937 gen32;
+
+	for (uint8_t i = 0; i < sc_TestCount; i++)
+	{
+		Value value = gen32();
+		uint8_t index = std::uniform_int_distribution<>(0, 31)(gen32);
+		uint8_t size = std::uniform_int_distribution<>(1, sc_Size - index)(gen32);
+		Value subValue = gen32();
+		EXPECT_EQ(Bits::SetAt(value, index, size, subValue), SetBitsetAt(value, index, size, subValue));
 	}
 }
